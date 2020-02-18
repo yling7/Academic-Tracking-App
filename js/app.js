bbca1e9aa22c279fa1fe327788726cb674974d14
brandenrbasche <bbasche@sundevils.asu.edu>
2020-02-17 18:11:45 -0700

    Added dashboard file, wrote more form validation in javascript, and linked pages together

--- js/app.js
+++ js/app.js
@@ -11,13 +11,13 @@ function registerOnSubmit() {
     let confirm = $("#confirm").val();
     let responses = [user, uni, fname, lname, pass, confirm];
 
-    for(var i=0; i<responses.length; i++) {
-        if(pass != confirm) {
+    for (var i = 0; i < responses.length; i++) {
+        if (pass != confirm) {
             alert("Passwords don't match.");
             break;
         }
 
-        if(responses[i] == "") {
+        if (responses[i] == "") {
             alert("Please complete all fields.");
             break;
         } else {
@@ -31,7 +31,8 @@ function registerOnSubmit() {
 
 // Put data into database
 function submitSignUpData(responses) {
-    // enter code here
+    alert("Succesfully inputted " + responses + " to the database.");
+    window.location.href = "./dashboard.html";
 }
 
 /* 
@@ -48,8 +49,40 @@ function submitGradeEntryData() {
     let course = $("#course").val();
     let grade = $("#grade").val();
     let required = [school, year, term, major, cumGpa, termGpa];
-    // let fullGrades = required.push(course, grade);
-    console.log(required);
-    console.log(fullGrades);
-}
+    let fullGrades = [school, year, term, major, cumGpa, termGpa, course, grade];
+    let optional = false;
 
+    if (fullGrades[6] == "") {
+        //handle required
+        for (var i = 0; i < required.length; i++) {
+            if (required[i] == "") {
+                alert("Please complete all fields.");
+                break;
+            } else if (required[4] > 4 || required[5] > 4) {
+                alert("GPA cannot be over 4.0.");
+                break;
+            } else {
+                submitSignUpData(required);
+                break;
+            }
+        }
+    } else {
+        // handle fullGrades
+        for(var i = 0; i < fullGrades.length; i++) {
+            if (fullGrades[i] == "") {
+                alert("Please complete all fields.");
+                break;
+            } else if (fullGrades[4] > 4 || fullGrades[5] > 4) {
+                alert("GPA cannot be over 4.0.");
+                break;
+            } else if(fullGrades[7] > 100) {
+                alert("Grade cannot be over 100.");
+                break;
+            } else {
+                submitSignUpData(fullGrades);
+                break;
+            }
+        }
+
+    }
+}
